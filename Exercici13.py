ffccbd8f5558aee5fbc30ada292fa6ec8d79efbf
psarsanedas <psarsanedas@institutmvm.cat>
2021-01-28 11:54:55 +0100

    New exercise

--- Exercici13.py
+++ Exercici13.py
@@ -6,7 +6,7 @@ def main():
     while num2 < 0:
         num2 = int(input("Introdueix el segon número: "))
     num1, num2 = num2, num1
-    print("Els valors s'han intercanviat, el resultat es aquest: ", num1, num2)
+    print("Els valors s'han intercanviat, el resultat es el següent: ", num1, num2)
 
 if __name__ == "__main__":
     main()
