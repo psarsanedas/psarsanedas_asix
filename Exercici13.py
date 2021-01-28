5eeb22977a8ab051f121bbdd191337aea549a0d8
psarsanedas <psarsanedas@institutmvm.cat>
2021-01-28 11:54:30 +0100

    Check exercise

---
+++ Exercici13.py
@@ -0,0 +1,12 @@
+def main():
+    num1 = int(input("Introdueix el primer número: "))
+    while num1 < 0:
+        num1 = int(input("Introdueix el primer número: "))
+    num2 = int(input("Introdueix el segon número: "))
+    while num2 < 0:
+        num2 = int(input("Introdueix el segon número: "))
+    num1, num2 = num2, num1
+    print("Els valors s'han intercanviat, el resultat es aquest: ", num1, num2)
+
+if __name__ == "__main__":
+    main()
